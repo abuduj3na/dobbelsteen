23fe0dc3e8731dd88cd4e15f8ede4ffbe930a363
MericUcarPXL <meric.ucar@icloud.com>
2024-10-19 22:39:59 +0200

    oefening klaar

--- dobbelsteen/MainWindow.xaml.cs
+++ dobbelsteen/MainWindow.xaml.cs
@@ -19,6 +19,36 @@ namespace dobbelsteen
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        int number = 0;
+        Random random = new Random();
+        int numberRoll = 0;
+
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            while(number != 6)
+            {
+                numberRoll++;
+                number = random.Next(0,7);
+                rollTextBox.Text += $"Worp {numberRoll} geeft " + $"{number}\n";
+            }
+        }
+
+        private void repeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            rollTextBox.Clear();
+            number = 0;
+            numberRoll = 0;
+            number++;
+            number = random.Next(0,7);
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
